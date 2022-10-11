d456516991f554dceb0ba75bf0c39d60cc108bcb
pinchies <pinchies@gmail.com>
2022-10-12 02:12:54 +1100

    first commit - code upload
    first commit - code upload

---
+++ lights_settone.py
@@ -0,0 +1,20 @@
+# Bluetooth device name: "YONGNUO LED"
+deviceUUID = "F67B6113-9A20-4C78-8D2A-9C68BE3DD3D5"  # you need to use the lights_scanbt.py script to get the correct UUID for your lights.
+primaryServiceUuid = 'f000aa60-0451-4000-b000-000000000000'
+receiveCharUuid = 'f000aa63-0451-4000-b000-000000000000'
+sendCharUuid = 'f000aa61-0451-4000-b000-000000000000'
+
+# connect to LED bluetooth light
+import asyncio
+import sys
+from bleak import BleakClient
+
+LED_warm = int(sys.argv[1]) # warm value 0-255
+LED_cool = int(sys.argv[2]) # cold value 0-255
+
+async def main():
+    async with BleakClient(deviceUUID) as client:
+        # Read a characteristic, etc.
+        await client.write_gatt_char(sendCharUuid, bytearray([0xae,0xaa,1,LED_cool,LED_warm,0x56]), response=True)
+
+asyncio.run(main())
